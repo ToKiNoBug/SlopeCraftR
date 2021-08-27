f55871ce0345dbf4d851ea77e0b27e72b30693b2
Van_Nya <nya@ruavan.one>
2021-08-27 22:09:36 +0800

    A better constant name

--- slopecraftr/constants/basic.py
+++ slopecraftr/constants/basic.py
@@ -4,5 +4,5 @@ PACKAGE_NAME = 'SlopeCraftR'
 VERSION = str(SEMVER := VersionInfo(major=0, minor=0, patch=0, prerelease='', build=''))
 
 AUTHOR = 'Van_Nya'
-GITHUB_REPO = f'https://github.com/Van-Nya/SlopeCraftR'
+REPOSITORY = f'https://github.com/Van-Nya/SlopeCraftR'
 DESCRIPTION = 'Get your 3D map pixel art in minecraft'
