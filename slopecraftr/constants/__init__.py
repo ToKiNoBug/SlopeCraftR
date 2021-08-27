2c99a954c31770afab4e21cdf13f8eb8a96a2699
Van_Nya <nya@ruavan.one>
2021-08-28 02:13:23 +0800

    Remove test statement

--- slopecraftr/constants/__init__.py
+++ slopecraftr/constants/__init__.py
@@ -6,10 +6,3 @@ __all__ = [
     'VERSION', 'PYTHON_REQUIRED',
     'AUTHOR', 'REPOSITORY', 'DESCRIPTION'
 ]
-
-if __name__ == '__main__':
-    from inspect import getmembers
-    from slopecraftr.constants import basic
-    members = dict(getmembers(basic))
-    for _ in __all__:
-        print(f'{_}: {members[_]}')
