99640fe65bc4819d2b2b57826569400d381b7e3c
Van_Nya <nya@ruavan.one>
2021-08-28 16:06:19 +0800

    Nicer structure

--- slopecraftr/constants/basic.py
+++ slopecraftr/constants/basic.py
@@ -5,8 +5,17 @@ NAME = 'SlopeCraftR'
 PACKAGE_NAME = NAME.lower()
 
 # Version Info
-VERSION = VersionInfo(major=0, minor=0, patch=0, prerelease='', build='')
-PYTHON_REQUIRED = VersionInfo(major=3, minor=8)
+VERSION = VersionInfo(
+    major=0,
+    minor=0,
+    patch=0,
+    prerelease='',
+    build=''
+)
+PYTHON_REQUIRED = VersionInfo(
+    major=3,
+    minor=8
+)
 
 # Project Info
 AUTHOR = 'Van_Nya'
