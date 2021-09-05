ac74ca33f6056ba74e3779aa7b9ed0deab89a42e
Van_Nya <nya@ruavan.one>
2021-09-05 23:33:21 +0800

    Add core of scr

---
+++ slopecraftr/core/_core.py
@@ -0,0 +1,9 @@
+from slopecraftr.utils.translation_manager import TranslationManager
+
+
+class SlopeCraftRCore:
+    def __init__(self):
+        self.translate = TranslationManager()
+
+
+core = SlopeCraftRCore()
